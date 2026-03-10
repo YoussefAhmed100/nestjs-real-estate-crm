d300f1179799032c067c516a22ce73c2f4b0bd79
ahmed-elshazli <ahmedshazli586@gmail.com>
2026-03-10 22:47:46 +0200

    Update allowed-file-types.constant.ts

--- src/common/storage/constants/allowed-file-types.constant.ts
+++ src/common/storage/constants/allowed-file-types.constant.ts
@@ -2,4 +2,10 @@ export const ALLOWED_IMAGE_TYPES = [
   'image/jpeg',
   'image/png',
   'image/webp',
+  'image/gif',
+  'image/bmp',
+  'image/tiff',
+  'image/avif',
+  'image/heic',
+  'image/heif',
 ];
